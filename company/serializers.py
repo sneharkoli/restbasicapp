76dd752b0ca7db6c4fd357ad207e4101d25ce2c1
Snehar Koli <76110873+sneharkoli@users.noreply.github.com>
2022-07-27 16:29:39 +0530

    Update serializers.py

--- company/serializers.py
+++ company/serializers.py
@@ -4,4 +4,4 @@ from .models import Company
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
-        fields = ['id', 'company_name', 'email_id', 'company_code', 'strength', 'strength', 'created_time']
+        fields = ['id', 'company_name', 'email_id', 'company_code', 'strength', 'website', 'created_time']
